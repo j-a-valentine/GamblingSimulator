06e436e77d52c9cfa6537e3c165186661f63e8c5
jakevalentine <j.a.valentine@wustl.edu>
2022-03-29 20:26:00 -0500

    Display bets works

--- src/simulator/Main.java
+++ src/simulator/Main.java
@@ -4,14 +4,12 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		
 		Roulette r = new Roulette();
 		r.addBet(100, 'o');
 		r.addBet(1, 0);
 		r.addBet(15, 11);
 		r.addBet(6, 20);
-		//r.displayBets();
+		r.displayBets();
 		r.play();
 	}
 
