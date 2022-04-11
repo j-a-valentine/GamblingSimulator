5414f811d8fefa2d76f1c79c97938448b21acfaf
jakevalentine <j.a.valentine@wustl.edu>
2022-04-11 15:05:09 -0500

    added tests

--- src/simulator/GamblingSystemTest.java
+++ src/simulator/GamblingSystemTest.java
@@ -5,6 +5,48 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class GamblingSystemTest {
+	
+	
+	
+	@Test
+	void testUserGoodRouletteBetNumber() {
+		GamblingSystem system = new GamblingSystem();
+		String [] input = {"10.23", "7"};
+		int ret = system.isValidRouletteBet(input);
+		assertEquals(1,ret, 0);
+	}
+	
+	@Test
+	void testUserGoodRouletteBetOdd() {
+		GamblingSystem system = new GamblingSystem();
+		String [] input = {"12.43", "o"};
+		int ret = system.isValidRouletteBet(input);
+		assertEquals(2,ret, 0);
+	}
+	
+	@Test
+	void testUserGoodRouletteBetEven() {
+		GamblingSystem system = new GamblingSystem();
+		String [] input = {"100.08", "e"};
+		int ret = system.isValidRouletteBet(input);
+		assertEquals(2,ret, 0);
+	}
+	
+	@Test
+	void testUserBadRouletteBetAmount() {
+		GamblingSystem system = new GamblingSystem();
+		String [] input = {"badinput", "7"};
+		int ret = system.isValidRouletteBet(input);
+		assertEquals(0,ret, 0);
+	}
+	
+	@Test
+	void testUserBadRouletteBetLocation() {
+		GamblingSystem system = new GamblingSystem();
+		String [] input = {"10", "badinput"};
+		int ret = system.isValidRouletteBet(input);
+		assertEquals(0,ret, 0);
+	}
 
 	@Test
 	void testGamblingWithdraw() {
@@ -24,6 +66,7 @@ class GamblingSystemTest {
 		
 		assertFalse(playDecision);
 	}
+	
 	
 
 
