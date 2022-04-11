a95e076d80a9d5ff6ae479f377cae689b5c742e3
jakevalentine <j.a.valentine@wustl.edu>
2022-04-11 13:05:25 -0500

    Changed Main

--- src/simulator/Main.java
+++ src/simulator/Main.java
@@ -4,14 +4,9 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		Roulette r = new Roulette();
-		r.addBet(100, 'o');
-		r.addBet(1, 0);
-		r.addBet(15, 11);
-		r.addBet(6, 20);
-		r.addBet(300000, 'e');
-		r.displayBets();
-		r.play();
+		GamblingSystem g = new GamblingSystem();
+		
+		g.runRoulette();
 	}
 
 }
