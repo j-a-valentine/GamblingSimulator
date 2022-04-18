124674afe6f0c1793803866d72c13205ae0db243
shengguo8 <g,sheng@wustl.edu>
2022-04-18 13:41:57 -0500

    done with card

---
+++ src/simulator/Card.java
@@ -0,0 +1,19 @@
+package simulator;
+
+public class Card{
+	private String display;
+	private int value;
+
+	public Card() {
+		this.value = value;
+		this.display = display;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+	
+	public String getDisplay() {
+		return this.display;
+	}
+}
