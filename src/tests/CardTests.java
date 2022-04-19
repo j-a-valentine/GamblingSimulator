29ce331b5dc2006884ef7e475db78b12b7f22c8c
jakevalentine <j.a.valentine@wustl.edu>
2022-04-18 22:50:19 -0500

    small syntax fixes

--- src/tests/CardTests.java
+++ src/tests/CardTests.java
@@ -3,13 +3,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import simulator.Card;
+
 class CardTests {
+	@Test
 	void testGetValue() {
-		Card three = new Card (3, '3');
-		assertEquals(3, three.getValue(), .001);
+		Card three = new Card (3, "3");
+		int value = three.getValue();
+		assertEquals(3, value, 0);
 	}
+	@Test
 	void testGetDisplay() {
-		Card three = new Card (3, '3');
-		assertTrue('3', three.getDisplay());
+		Card three = new Card (3, "3");
+		assertTrue(three.getDisplay().equals("3"));
 	}
 }
