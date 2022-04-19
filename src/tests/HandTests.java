0a0055d068dd10e29248f4326572ffae4a3d51f3
jakevalentine <j.a.valentine@wustl.edu>
2022-04-18 20:47:14 -0500

    evaluate works

--- src/tests/HandTests.java
+++ src/tests/HandTests.java
@@ -20,5 +20,41 @@ class HandTests {
 		assertEquals(bsize+1, asize, 0);
 		
 	}
+	@Test
+	void testEvaluate() {
+		Hand h = new Hand(false);
+		h.addCard(new Card(10, "J"));
+		h.addCard(new Card(11, "A"));
+		int eval = h.evaluate();
+		assertEquals(21, eval, 0);
+	}
+	@Test
+	void testEvaluateOneAce() {
+		Hand h = new Hand(false);
+		h.addCard(new Card(10, "J"));
+		h.addCard(new Card(11, "A"));
+		h.addCard(new Card(5, "5"));
+		int eval = h.evaluate();
+		assertEquals(16, eval, 0);
+	}
+	@Test
+	void testEvaluateTwoAce() {
+		Hand h = new Hand(false);
+		h.addCard(new Card(9, "9"));
+		h.addCard(new Card(11, "A"));
+		h.addCard(new Card(11, "A"));
+		int eval = h.evaluate();
+		assertEquals(21, eval, 0);
+	}
+	@Test
+	void testEvaluateOver21() {
+		Hand h = new Hand(false);
+		h.addCard(new Card(10, "10"));
+		h.addCard(new Card(10, "J"));
+		h.addCard(new Card(11, "A"));
+		h.addCard(new Card(4, "4"));
+		int eval = h.evaluate();
+		assertEquals(-1, eval, 0);
+	}
 
 }
